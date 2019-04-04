1115d60587a4c2acbafacce37213f3ff89cbb948
anggitafania <36650637+anggitafania@users.noreply.github.com>
2019-04-05 00:12:19 +0700

    sudah ada bank statement, tapi masih ada bug

--- ATM_New/src/com/appl/atm/model/Withdrawal.java
+++ ATM_New/src/com/appl/atm/model/Withdrawal.java
@@ -18,56 +18,63 @@ public class Withdrawal extends Transaction {
 
     // Withdrawal constructor
     public Withdrawal(int userAccountNumber, BankDatabase atmBankDatabase,
-	    CashDispenser atmCashDispenser) {
+            CashDispenser atmCashDispenser) {
 
-	// initialize superclass variables
-	super(userAccountNumber, atmBankDatabase);
-	cashDispenser = atmCashDispenser;
+        // initialize superclass variables
+        super(userAccountNumber, atmBankDatabase);
+        cashDispenser = atmCashDispenser;
     }
 
     @Override
     public int execute() {
-	Account account = getBankDatabase().getAccount(getAccountNumber());
+        Account account = getBankDatabase().getAccount(getAccountNumber());
 
-	if (account.getAvailableBalance() < amount) {
-	    return BALANCE_NOT_ENOUGH;
-	}
+        if (account.getAvailableBalance() < amount) {
+            return BALANCE_NOT_ENOUGH;
+        }
 
-	if (cashDispenser.isSufficientCashAvailable(amount)) {
-	    cashDispenser.dispenseCash(amount);
-	    account.debit(amount);
-	    return WITHDRAW_SUCCESSFUL;
-	} else {
-	    return CASHDISPENSER_NOT_ENOUGH;
-	}
+        if (cashDispenser.isSufficientCashAvailable(amount)) {
+            cashDispenser.dispenseCash(amount);
+            account.debit(amount);
+//            getBankDatabase().addBankStatement(new Statement(this, WITHDRAWAL));
+            return WITHDRAW_SUCCESSFUL;
+        } else {
+            return CASHDISPENSER_NOT_ENOUGH;
+        }
     }
 
     /**
      * @return the amount
      */
     public int getAmount() {
-	return amount;
+        return amount;
     }
 
     /**
      * @param amount the amount to set
      */
     public void setAmount(int amount) {
-	this.amount = amount;
+        this.amount = amount;
     }
 
     /**
      * @return the cashDispenser
      */
     public CashDispenser getCashDispenser() {
-	return cashDispenser;
+        return cashDispenser;
     }
 
     /**
      * @param cashDispenser the cashDispenser to set
      */
     public void setCashDispenser(CashDispenser cashDispenser) {
-	this.cashDispenser = cashDispenser;
+        this.cashDispenser = cashDispenser;
+    }
+
+    @Override
+    public String toString() {
+        String res = "Withdraw $" + amount + ".";
+        return res;
     }
 
 }
