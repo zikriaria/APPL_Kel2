ed62bea63e139d13c983deeda8509bc4565145c5
anggitafania <36650637+anggitafania@users.noreply.github.com>
2019-04-12 00:34:22 +0700

    ini YANG FIX

--- ATM_New/src/com/appl/atm/controller/AdminMode.java
+++ ATM_New/src/com/appl/atm/controller/AdminMode.java
@@ -8,6 +8,8 @@ package com.appl.atm.controller;
 import com.appl.atm.model.Account;
 import com.appl.atm.model.BankDatabase;
 import com.appl.atm.model.CashDispenser;
+import com.appl.atm.model.Transaction;
+import com.appl.atm.model.ValidateDeposit;
 import com.appl.atm.view.Keypad;
 import com.appl.atm.view.Screen;
 
@@ -21,11 +23,13 @@ public class AdminMode {
     private static final int UNBLOCK_ACCOUNT = 2;
     private static final int VIEW_DISPENSER = 3;
     private static final int ADD_DISPENSER = 4;
-    private static final int QUIT = 5;
+    private static final int VALIDATE_DEPOSIT = 5;
+    private static final int QUIT = 6;
     private BankDatabase bankDatabase;
     private CashDispenser cashDispenser;
     private Keypad keypad;
     private Screen screen;
+    private int currentAccountNumber;
 
     AdminMode(BankDatabase paramBankDatabase, CashDispenser paramCashDispenser) {
         bankDatabase = paramBankDatabase;
@@ -35,14 +39,19 @@ public class AdminMode {
     }
 
     public void execute() {
+
+        Transaction currentTransaction = null;
+        TransactionController currentTransactionController = null;
+
         int option = 0;
-        while (option != 5) {
+        while (option != 6) {
             screen.displayMessageLine("Admin Mode");
             screen.displayMessageLine("1. Add account");
             screen.displayMessageLine("2. Unblock account");
             screen.displayMessageLine("3. View money from Dispenser");
             screen.displayMessageLine("4. Add money to Dispenser");
-            screen.displayMessageLine("5. Quit");
+            screen.displayMessageLine("5. Validasi Deposit");
+            screen.displayMessageLine("6. Quit");
             screen.displayMessage("Choose an option number: ");
             option = keypad.getInput();
             switch (option) {
@@ -58,11 +67,14 @@ public class AdminMode {
                     double addedAvailableBalance = keypad.getInput();
                     screen.displayMessage("Insert total balance: ");
                     double addedTotalBalance = keypad.getInput();
-                    bankDatabase.addAccount(addedAccountNumber, addedAccountPIN, addedAvailableBalance, addedTotalBalance,addedNewType);
+                    bankDatabase.addAccount(addedAccountNumber, addedAccountPIN, addedAvailableBalance, addedTotalBalance, addedNewType);
                     screen.displayMessageLine("New account has been added\n");
                     break;
                 case UNBLOCK_ACCOUNT:
-                    screen.displayMessageLine("tes unblock account");
+                    screen.displayMessage("Enter the account number to unblock: ");
+                    int accountNumber = keypad.getInput();
+                    bankDatabase.getAccount(accountNumber).setBlocked(false);
+                    screen.displayMessageLine("The account has been unblocked.\n");
                     break;
                 case VIEW_DISPENSER:
                     cashDispenser.displayDispenser();
@@ -73,6 +85,14 @@ public class AdminMode {
                     double add = keypad.getInput();
                     cashDispenser.addCashDispenser((int) add);
 //              screen.displayMessageLine("add dispenser");
+                    break;
+                case VALIDATE_DEPOSIT:
+                    currentTransaction
+                            = new ValidateDeposit(
+                                    currentAccountNumber, screen, bankDatabase, keypad);
+                    currentTransactionController
+                            = new ValidateDepositController(currentTransaction, keypad, screen);
+                    currentTransactionController.run(); // execute transaction
                     break;
                 case QUIT:
                     screen.displayMessageLine("Quit....");
